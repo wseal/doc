cd8e9dbbad9c4c988f8bdff70506e63938ba695d
seal <wxiaowar@gmail.com>
2023-04-24 20:49:57 +0800

    program function navigate

--- code/hello-router/src/pages/Message.jsx
+++ code/hello-router/src/pages/Message.jsx
@@ -1,29 +1,42 @@
 import React, { useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 
 export default function Message() {
+  const navigate = useNavigate();
+
   const [messages] = useState([
     {
       id: "001",
       title: "message1",
-      contnt: "白日依山尽",
+      content: "白日依山尽",
     },
     {
       id: "002",
       title: "message2",
-      contnt: "黄河入海流",
+      content: "黄河入海流",
     },
     {
       id: "003",
       title: "message3",
-      contnt: "欲穷千里目",
+      content: "欲穷千里目",
     },
     {
       id: "004",
       title: "message4",
-      contnt: "更上一层楼",
+      content: "更上一层楼",
     },
   ]);
+
+  function showDetail(m) {
+    navigate("detail", {
+      replace: false,
+      state: {
+        id: m.id,
+        title: m.title,
+        content: m.content,
+      },
+    });
+  }
   return (
     <div>
       <ul>
@@ -32,10 +45,11 @@ export default function Message() {
             <li key={m.id}>
               <Link
                 to="detail"
-                state={{ id: m.id, title: m.title, content: m.contnt }}
+                state={{ id: m.id, title: m.title, content: m.content }}
               >
                 {m.title}
               </Link>
+              <button onClick={() => showDetail(m)}>click</button>
             </li>
           );
         })}
