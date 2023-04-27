e60f7d86feda1b7f2d6fae72b1138107163db237
seal <wxiaowar@gmail.com>
2023-04-27 18:27:34 +0800

    use vite-plugin import antd less when need

--- code/hello-admin/vite.config.ts
+++ code/hello-admin/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
-// yarn add @types/node
+import path from "path"; // yarn add @types/node -D
+import { createStyleImportPlugin, AntdResolve } from "vite-plugin-style-import";
+// yarn add consola -D
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), createStyleImportPlugin({ resolves: [AntdResolve()] })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
