4b8c1351b429e3db3edfae2b9b0a1e3afb5bf305
seal <wxiaowar@gmail.com>
2023-04-28 18:54:47 +0800

    use redux keep status

--- code/hello-redux/src/components/Count/index.jsx
+++ code/hello-redux/src/components/Count/index.jsx
@@ -1,44 +1,59 @@
 import React, { Component } from "react";
 
+import store from "../../redux/store";
+
 export default class Count extends Component {
-  state = { count: 0 };
+  state = { message: "win" };
+
+  componentDidMount() {
+    // monitor redux value update
+    store.subscribe(() => {
+      this.setState({});
+    });
+  }
 
   increment = () => {
     const { value } = this.selectNumber;
-    const { count } = this.state;
-    this.setState({ count: count + value * 1 });
+    // const { count } = this.state;
+    // this.setState({ count: count + value * 1 });
+    // 分发事件
+    store.dispatch({ type: "increment", data: value * 1 });
   };
 
   decrement = () => {
     const { value } = this.selectNumber;
-    const { count } = this.state;
-    this.setState({ count: count - value * 1 });
+    // const { count } = this.state;
+    // this.setState({ count: count - value * 1 });
+    store.dispatch({ type: "decrement", data: value * 1 });
   };
 
   incrementIfOdd = () => {
     const { value } = this.selectNumber;
-    const { count } = this.state;
+    // const { count } = this.state;
+    const count = store.getState();
     if (count % 2 === 0) {
       return;
     }
 
-    this.setState({ count: count + value * 1 });
+    // this.setState({ count: count + value * 1 });
+    store.dispatch({ type: "increment", data: value * 1 });
   };
 
   incrementAsync = () => {
     const { value } = this.selectNumber;
-    const { count } = this.state;
+    // const { count } = this.state;
 
     setTimeout(() => {
-      this.setState({ count: count + value * 1 });
+      // this.setState({ count: count + value * 1 });
+      store.dispatch({ type: "increment", data: value * 1 });
     }, 500);
   };
 
   render() {
-    const { count } = this.state;
+    // const { count } = this.state;
     return (
       <div>
-        <h1>current sum:{count}</h1>
+        <h1>current sum:{store.getState() /*get value */}</h1>
         <select ref={(c) => (this.selectNumber = c)}>
           <option value="1">1</option>
           <option value="2">2</option>
