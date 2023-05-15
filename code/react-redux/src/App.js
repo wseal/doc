9d8b5634733e6c42976ac1166ae8783444caa68d
seal <wxiaowar@gmail.com>
2023-05-15 19:54:54 +0800

    use provider to deliver store to all component

--- code/react-redux/src/App.js
+++ code/react-redux/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 
 import Counter from "./container/Counter";
-import store from "./redux/store";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Counter store={store} />
+        <Counter />
       </div>
     );
   }
