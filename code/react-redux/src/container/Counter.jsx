4260ee25c6608b2a2988f43b12e644b37a15eb7a
seal <wxiaowar@gmail.com>
2023-05-15 20:01:58 +0800

    move ui component into container file

--- code/react-redux/src/container/Counter.jsx
+++ code/react-redux/src/container/Counter.jsx
@@ -1,12 +1,55 @@
+import React, { Component } from "react";
 import { connect } from "react-redux";
-
-import CountUI from "../components/Count";
 
 import {
   createAsyncIncAction,
   createDecAction,
   createIncAction,
 } from "../redux/count_action";
+
+class Count extends Component {
+  state = { message: "win" };
+
+  increment = () => {
+    const { value } = this.selectNumber;
+    this.props.add(value * 1);
+  };
+
+  decrement = () => {
+    const { value } = this.selectNumber;
+    this.props.desc(value * 1);
+  };
+
+  incrementIfOdd = () => {
+    const { value } = this.selectNumber;
+    if (value % 2 !== 0) {
+      this.props.add(value * 1);
+    }
+  };
+
+  incrementAsync = () => {
+    const { value } = this.selectNumber;
+    this.props.asyncAdd(value * 1, 500);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>current sum:{this.props.count}</h1>
+        <select ref={(c) => (this.selectNumber = c)}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+        &nbsp;
+        <button onClick={this.increment}>+</button>&nbsp;
+        <button onClick={this.decrement}>-</button>&nbsp;
+        <button onClick={this.incrementIfOdd}>when odd, +</button>&nbsp;
+        <button onClick={this.incrementAsync}>async add</button>&nbsp;
+      </div>
+    );
+  }
+}
 
 // 函数返回一个对象
 // 对象的key value 作为props传给ui组件
@@ -23,11 +66,11 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(CountUI);
+export default connect(mapStateToProps, mapDispatchToProps)(Count);
 
 // 简写形式
 // export default connect((state) => ({ count: state }), {
 //   add: createIncAction,
 //   desc: createDecAction,
 //   asyncAdd: createAsyncIncAction,
-// })(CountUI);
+// })(Count);
