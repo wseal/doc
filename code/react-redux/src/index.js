888d43063c4754235332d0250b3a8d284b9040b6
seal <wxiaowar@gmail.com>
2023-05-15 19:48:41 +0800

    react-redux no need use subscribe

--- code/react-redux/src/index.js
+++ code/react-redux/src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import store from "./redux/store";
+// import store from "./redux/store";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-store.subscribe(() => {
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-});
+// store.subscribe(() => {
+//   root.render(
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   );
+// });
 
 root.render(
   <React.StrictMode>
